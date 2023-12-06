636ef4b18b81e1e99a258f330cd20b3131782d2d
yluo12 <yantingluo98@gmail.com>
2023-12-06 14:09:57 -0800

    added sass package

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import Hero from '@components/Hero';
 import Projects from '@components/Projects';
 import Skills from '@components/Skills';
+import Games from '@components/Games';
 import Contact from '@components/Contact';
 import Footer from '@components/Footer';
 
@@ -10,6 +11,7 @@ const Home = () => {
       <Hero />
       <Skills />
       <Projects />
+      <Games />
       <Contact />
       <Footer />
     </div>
---
+++ components/Games.jsx
@@ -0,0 +1,10 @@
+const Games = () => {
+  return (
+    <section>
+      <h2>NOSTALGIC GAMES</h2>
+      <p>Coming soon!</p>
+    </section>
+  )
+};
+
+export default Games;
